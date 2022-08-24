6be8d49fa687c327b95554cb9becb98715304e2f
emoacht <emotom@pobox.com>
2022-08-24 21:10:17 +0900

    Add image update test project

---
+++ Source/WpfImageUpdate/ImageHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.Windows;
+
+namespace WpfImageUpdate
+{
+	internal static class ImageHelper
+	{
+		public static void RenderThenCopyBitmap(FrameworkElement element, ref WriteableBitmap? writeableBitmap)
+		{
+			if (element is null)
+				return;
+
+			var dpi = VisualTreeHelper.GetDpi(element);
+			int width = (int)(element.ActualWidth * dpi.DpiScaleX);
+			int height = (int)(element.ActualHeight * dpi.DpiScaleY);
+
+			var renderBitmap = new RenderTargetBitmap(
+				width,
+				height,
+				dpi.PixelsPerInchX,
+				dpi.PixelsPerInchY,
+				PixelFormats.Pbgra32);
+
+			renderBitmap.Render(element);
+			renderBitmap.Freeze();
+
+			if (writeableBitmap is null)
+			{
+				writeableBitmap = new WriteableBitmap(renderBitmap);
+				return;
+			}
+
+			var rect = new Int32Rect(0, 0, width, height);
+			int stride = width * 4; // 32 bits per pixel
+			var buffer = new byte[stride * height];
+
+			renderBitmap.CopyPixels(rect, buffer, stride, 0);
+			renderBitmap.Clear();
+
+			writeableBitmap.WritePixels(rect, buffer, stride, 0);
+		}
+	}
+}
---
+++ Source/WpfImageUpdate/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace WpfImageUpdate
+{
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			this.Loaded += OnLoaded;
+			this.SizeChanged += OnSizeChanged;
+		}
+
+		private void OnLoaded(object sender, RoutedEventArgs e)
+		{
+			ResetBitmap();
+		}
+
+		private void OnSizeChanged(object sender, SizeChangedEventArgs e)
+		{
+			if (this.IsLoaded)
+				ResetBitmap();
+		}
+
+		protected override void OnDpiChanged(DpiScale oldDpi, DpiScale newDpi)
+		{
+			base.OnDpiChanged(oldDpi, newDpi);
+
+			if (this.IsLoaded)
+				ResetBitmap();
+		}
+
+		private WriteableBitmap? _writeableBitmap;
+
+		private void ResetBitmap()
+		{
+			_writeableBitmap = null;
+			ImageHelper.RenderThenCopyBitmap(canvas, ref _writeableBitmap);
+			this.image.Source = _writeableBitmap;
+		}
+
+		private Point? _positionToBall;
+
+		private void Ball_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			var ball = (UIElement)sender;
+			_positionToBall = e.GetPosition(ball);
+		}
+
+		private void Ball_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+		{
+			_positionToBall = null;
+		}
+
+		private void Ball_MouseLeave(object sender, MouseEventArgs e)
+		{
+			_positionToBall = null;
+		}
+
+		private void Ball_MouseMove(object sender, MouseEventArgs e)
+		{
+			if (!_positionToBall.HasValue)
+				return;
+			
+			var ball = (UIElement)sender;
+			var positionToCanvas = e.GetPosition(canvas);
+			
+			Canvas.SetLeft(ball, positionToCanvas.X - _positionToBall.Value.X);
+			Canvas.SetTop(ball, positionToCanvas.Y - _positionToBall.Value.Y);
+			
+			ImageHelper.RenderThenCopyBitmap(canvas, ref _writeableBitmap);
+		}
+	}
+}
